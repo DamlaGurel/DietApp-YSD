2940defe97729b961a8be57a7f2d660a4269d204
Yalın <yalintuzmen08@gmail.com>
2024-02-10 03:44:00 +0300

    Kategori Kıyas Raporu

--- DietApp/DietApp.BLL.Services/RaporlarService.cs
+++ DietApp/DietApp.BLL.Services/RaporlarService.cs
@@ -1,5 +1,6 @@
 ﻿using DietApp.BLL.IServices;
 using DietApp.DAL.Repositories;
+using DietApp.Entities;
 using DietApp.ViewModels.Raporlar;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,14 @@ namespace DietApp.BLL.Services
     public class RaporlarService : IRaporlarService
     {
         OgunRepository _ogunRepo;
+        KategoriRepository _kategoriRepo;
+        KullaniciKisiselRepository _kisiselRepo;
 
         public RaporlarService()
         {
             _ogunRepo = new OgunRepository();
+            _kategoriRepo = new KategoriRepository();
+            _kisiselRepo = new KullaniciKisiselRepository();
         }
 
         public List<GunlukRaporVm> GunlukRapor(int id, DateTime gun)
@@ -25,6 +30,43 @@ namespace DietApp.BLL.Services
                 OgunAdi = x.OgunAdi,
                 Kalori = x.Kalori
             }).ToList(); return gunlukRapors;
+        }
+
+        public List<Kategori> KategoriGetAll()
+        {
+            return new KategoriRepository().GetAll().ToList();
+        }
+
+
+        public void KiyasRaporOgun(DateTime baslangicTarih, DateTime bitistarih, Kategori kat, int kisiID, out double GenelOrtalamaKalori, out double KisiOrtalamaKalori)
+        {
+            List<double> query = (List<double>)
+                from kisisel in _kisiselRepo.GetAll()
+                join ogun in _ogunRepo.GetAll() on kisisel.ID equals ogun.KullaniciKisiselID
+                join ymkogun in new YemekMiktarOgunRepository().GetAll() on ogun.ID equals ymkogun.OgunID
+                join miktar in new YemekMiktariRepository().GetAll() on ymkogun.YemekMiktarID equals miktar.ID
+                join yemek in new YemekRepository().GetAll() on miktar.YemekID equals yemek.ID
+                join kategori in new KategoriRepository().GetAll() on yemek.KategoriID equals kategori.ID
+                where kategori == kat
+                select new
+                {
+                    kalori = yemek.Kalori
+                };
+            GenelOrtalamaKalori = query.Average();
+
+            query = (List<double>)
+                from kisisel in _kisiselRepo.GetAll()
+                join ogun in _ogunRepo.GetAll() on kisisel.ID equals ogun.KullaniciKisiselID
+                join ymkogun in new YemekMiktarOgunRepository().GetAll() on ogun.ID equals ymkogun.OgunID
+                join miktar in new YemekMiktariRepository().GetAll() on ymkogun.YemekMiktarID equals miktar.ID
+                join yemek in new YemekRepository().GetAll() on miktar.YemekID equals yemek.ID
+                join kategori in new KategoriRepository().GetAll() on yemek.KategoriID equals kategori.ID
+                where kategori == kat && kisisel.ID == kisiID
+                select new
+                {
+                    kalori = yemek.Kalori
+                };
+            KisiOrtalamaKalori = query.Average();
         }
     }
 }
--- DietApp/DietApp.UI/Rapor/KiyasRaporlari.cs
+++ DietApp/DietApp.UI/Rapor/KiyasRaporlari.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DietApp.BLL.Services;
+using DietApp.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,28 @@ namespace DietApp.UI
 {
     public partial class KiyasRaporlari : Form
     {
+        int kisiID;
         public KiyasRaporlari(int id)
         {
             InitializeComponent();
+            kisiID = id;
+        }
+
+        private void cmbKategori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbKategori.SelectedItem == null) return;
+
+            double ortalamaKisi, ortalamaGenel;
+
+
+            new RaporlarService().KiyasRaporOgun(dtpBaslangicTarihi.Value.Date, dtpBitisTarihi.Value.Date,  
+(Kategori)cmbKategori.SelectedItem, kisiID, out ortalamaGenel, out ortalamaKisi);
+        }
+
+        private void KiyasRaporlari_Load(object sender, EventArgs e)
+        {
+            cmbKategori.DataSource = new RaporlarService().KategoriGetAll();
+            cmbKategori.DisplayMember = "KategoriAdi";
         }
     }
 }
